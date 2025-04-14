70a55bcf4ddf907f08c6ecee44274e063e91f482
YongMin Son <plusf91@gmail.com>
2025-04-14 19:41:33 +0900

    250414_생명주기

--- ProjectGame/Assets/Class2th (Script)/Scripts/Creature.cs
+++ ProjectGame/Assets/Class2th (Script)/Scripts/Creature.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
-    Stat creature = new();
+    //Stat creature = new();
+    private Stat stat2 = new Stat();
 
     public Creature()
     {
-        creature.name = "BearMonster";
-        creature.level = 10;
-        creature.headlth = 100;
-        Debug.Log("Creature");
-        Debug.Log(creature.name);
-        Debug.Log(creature.level);
-        Debug.Log(creature.headlth);
+        //creature.name = "BearMonster";
+        //creature.level = 10;
+        //creature.health = 100;
+        //Debug.Log("Creature");
+        //Debug.Log(creature.Name);
+        //Debug.Log(creature.Level);
+        //Debug.Log(creature.Health);
+
+        stat2.Level = 10;
+        stat2.Health = 100;
+        stat2.Name = "Bbbbb";
+        Debug.Log("Level"+ stat2.Level);
+        Debug.Log("Health" + stat2.Health);
+        Debug.Log("Name" + stat2.Name);
+
+        
     }
 }
+
--- ProjectGame/Assets/Class2th (Script)/Scripts/Stat.cs
+++ ProjectGame/Assets/Class2th (Script)/Scripts/Stat.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Stat //new
 {
-    public int level;
-    public int headlth;
-    public string name;
+    private int level;
+    private int health;
+    private string name;
+
+    public int Level 
+    { 
+        get { return level; } 
+        set 
+        { 
+            //if(value < 100)
+            level = value; 
+        } 
+    }
+    public int Health { get { return health; } set { health = value; } }
+    public string Name { get { return name; } set { name = value; } }
 }
