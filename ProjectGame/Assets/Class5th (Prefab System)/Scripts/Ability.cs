6110e3d72abc818d640296d751d2cbaf1a774848
YongMin Son <plusf91@gmail.com>
2025-04-16 19:42:15 +0900

    250416_prefabs

---
+++ ProjectGame/Assets/Class5th (Prefab System)/Scripts/Ability.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ability : MonoBehaviour
+{
+    [SerializeField] int strength = 10;
+
+    void Start()
+    {
+        Debug.Log("strength °ª : " + strength);
+
+    }
+
+}
