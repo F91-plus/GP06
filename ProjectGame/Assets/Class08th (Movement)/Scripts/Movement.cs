74ec6ed06a081f368d857337aaf9ed9aaffbe0c8
YongMin Son <plusf91@gmail.com>
2025-04-24 19:41:26 +0900

    250424_Collision

--- ProjectGame/Assets/Class08th (Movement)/Scripts/Movement.cs
+++ ProjectGame/Assets/Class08th (Movement)/Scripts/Movement.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
-    void Start()
-    {
-        transform.position = new Vector3(15,0,0);
-    }
+    [SerializeField] float speed = 5.0f;
+    [SerializeField] Vector3 direction;
 
     void Update()
     {
+        //direction.x = Input.GetAxis("Horizontal"); // 이것은 줄어드는 형식이라 잔여값으로 인해 조금 더 움직임
+        direction.x = Input.GetAxisRaw("Horizontal");
+        //direction.y = Input.GetAxis("Depth");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        direction.Normalize();
+
+        transform.position += direction * Time.deltaTime * speed;
+
+        Debug.Log(direction);   // (0.00, 0.00, 0.00)
+
+        /*
         if (Input.GetKeyDown(KeyCode.W))
         {
             transform.position += Vector3.forward;
+
+            //transform.position += new Vector3(0, 0, 1);
             
         }
         if (Input.GetKeyDown(KeyCode.S))
@@ -28,5 +42,6 @@ public class Movement : MonoBehaviour
         {
             transform.position += Vector3.right;
         }
+        */
     }
 }
---
+++ ProjectGame/Assets/Class09th (Collsion)/Scripts/Sensor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Senser : MonoBehaviour
+{
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        // 물리적이지 않은 충돌을 했을 때 호출되는 이벤트 함수
+        if (other.CompareTag("Authorized"))
+            Debug.Log("OT Enter");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        // 물리적이지 않은 충돌을 하고 있을 때 호출 되는 이벤트 함수
+
+        Debug.Log("OnTrg Stay");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // 물리적이지 않은 충돌이 끝났을 때 호출되는 이벤트 함수
+        Debug.Log("OnTrg Exit");
+    }
+}
