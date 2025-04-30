2cc331caf99ea080c199e4496d38dde955e7ccfa
YongMin Son <plusf91@gmail.com>
2025-04-30 19:42:07 +0900

    250430_Clone

--- ProjectGame/Assets/Class10th (Instantiate & Destroy)/Scripts/WeaponManager.cs
+++ ProjectGame/Assets/Class10th (Instantiate & Destroy)/Scripts/WeaponManager.cs
@@ -7,30 +7,58 @@ using UnityEngine.UIElements;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    [SerializeField] GameObject [] weapons;
+    [SerializeField] GameObject[] weapons;
     [SerializeField] List<GameObject> weaponsList;
-
-    //create posistion
     [SerializeField] Transform parentPosition;
+    [SerializeField] int count = 0;
 
-    // Start is called before the first frame update
+    private int number = 0;
+
     void Start()
     {
-        //Instantiate(prefab, parentPosition);//object, parant posistion
-
-        for (int i = 0; i < weapons.Length; i++) 
+        for (int i = 0; i < weapons.Length; i++)
         {
-            weaponsList[i] = Instantiate(weapons[i], parentPosition);
+            //weaponsList[i] = Instantiate(weapons[i], parentPosition);
+            GameObject clone = Instantiate(weapons[i], parentPosition);
+            clone.gameObject.SetActive(false);
+
+            weaponsList.Add(clone);
         }
+
+        weaponsList[number].SetActive(true);
+        //weaponsList[number].gameObject.SetActive(true);
     }
 
     private void Update()
     {
-        //Input.GetAxis("Jump")
-        //if ()
-        //{
+        //Input.GetKeyDown(KeyCode.Space) µµ °¡´É
+        if (Input.GetButtonDown("Jump"))
+        {
+            Swap();
+        }
 
-        //}
+        if (Input.GetMouseButtonDown(0))  // 0 ¿Þ, 1 ¿À
+        {
+            Attack();
+            //Knife knife = new Knife();
+            //knife.Attack();
+        }
     }
 
+    public void Swap()
+    {
+        weaponsList[number].SetActive(false);
+        number++;
+        if (number >= weaponsList.Count) { number = 0; }
+        weaponsList[number].SetActive(true);
+
+        //weaponsList[count].gameObject.SetActive(false);
+        //count = (count + 1) % weaponsList.Count;
+        //weaponsList[count].gameObejct.SetActive(true);
+    }
+
+    public void Attack()
+    {
+        weaponsList[number].GetComponent<Weapon>().Attack();
+    }
 }
